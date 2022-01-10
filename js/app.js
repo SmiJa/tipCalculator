27c5ad41950443430778260fb7aa12fdcd5e752b
Jason Smith <smija13@gmail.com>
2022-01-09 23:39:14 -0800

    Refactor code

--- js/app.js
+++ js/app.js
@@ -1,44 +1,30 @@
 const billAmount = document.getElementById('amount');
 const tipBox = document.getElementById('tip-box');
 const totalBox = document.getElementById('total-box');
-let percent15 = document.getElementById('percent15');
-let percent18 = document.getElementById('percent18');
-let percent20 = document.getElementById('percent20');
+const percent15 = document.getElementById('percent15');
+const percent18 = document.getElementById('percent18');
+const percent20 = document.getElementById('percent20');
+const percent15Value = percent15.value;
+const percent18Value = percent18.value;
+const percent20Value = percent20.value;
 let billValue;
-let percent = 0;
 let tipAmount = 0.00;
 let totalAmount = 0.00;
 
 tipBox.innerText = `$${tipAmount.toFixed(2)}`;
 totalBox.innerText = `$${totalAmount.toFixed(2)}`;
 
-const calcTipAmount = (bill, tipPercent) => {
-    return bill * tipPercent;
+
+const getTipAndTotal = (tippercent) => {
+    billValue = parseFloat(billAmount.value);
+    tipAmount = billValue * tippercent;
+    totalAmount = billValue + tipAmount;
+    tipBox.innerText = `$${tipAmount.toFixed(2)}`;
+    totalBox.innerText = `$${totalAmount.toFixed(2)}`;
 }
 
-percent15.addEventListener('change', function() {
-    billValue = parseFloat(billAmount.value);
-    percent = 0.15;
-    tipAmount = calcTipAmount(billValue, percent);
-    totalAmount = billValue + tipAmount;
-    tipBox.innerText = `$${tipAmount.toFixed(2)}`;
-    totalBox.innerText = `$${totalAmount.toFixed(2)}`;
-});
+percent15.addEventListener('change', () => getTipAndTotal(percent15Value));
 
-percent18.addEventListener('change', function() {
-    billValue = parseFloat(billAmount.value);
-    percent = 0.18;
-    tipAmount = calcTipAmount(billValue, percent);
-    totalAmount = billValue + tipAmount;
-    tipBox.innerText = `$${tipAmount.toFixed(2)}`;
-    totalBox.innerText = `$${totalAmount.toFixed(2)}`;
-});
+percent18.addEventListener('change', () => getTipAndTotal(percent18Value));
 
-percent20.addEventListener('change', function() {
-    billValue = parseFloat(billAmount.value);
-    percent = 0.2;
-    tipAmount = calcTipAmount(billValue, percent);
-    totalAmount = billValue + tipAmount;
-    tipBox.innerText = `$${tipAmount.toFixed(2)}`;
-    totalBox.innerText = `$${totalAmount.toFixed(2)}`;
-});
+percent20.addEventListener('change', () => getTipAndTotal(percent20Value));
